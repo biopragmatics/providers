bc6a60fb1ed03d445ed31346786edf506db5ce57
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-06-16 10:34:21 +0200

    Update pages

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 This repository contains static sites generated for resources
 that do not have their own providers, but are implemented in PyOBO.
 
-- [Selventa Complexes (`scomp`)](/scomp)
-- [Molecular Signatures Database (`msigdb`)](/msigdb)
-- [Selventa Families (`sfam`)](/sfam)
-- [UniProt Post-Translational Modification (`uniprot.ptm`)](/uniprot.ptm)
-- [Selventa Chemicals (`schem`)](/schem)
-- [Selventa Diseases (`sdis`)](/sdis)
+- [Selventa Families (`sfam`)](sfam)
+- [Selventa Chemicals (`schem`)](schem)
+- [Selventa Complexes (`scomp`)](scomp)
+- [UniProt Post-Translational Modification (`uniprot.ptm`)](uniprot.ptm)
+- [Selventa Diseases (`sdis`)](sdis)
+- [Molecular Signatures Database (`msigdb`)](msigdb)
 
 ## Update
 
@@ -17,3 +17,4 @@ The sites can be updated with the following:
 ```shell
 $ pip install tox
 $ tox
+```
--- generate.py
+++ generate.py
@@ -3,26 +3,29 @@
 from pathlib import Path
 
 import bioregistry
+from pyobo import Obo
 from pyobo.sources import ontology_resolver
 from pyobo.ssg import make_site
 import click
+from tqdm.contrib.logging import logging_redirect_tqdm
 
 HERE = Path(__file__).parent.resolve()
-
+REPOSITORY_NAME = "providers"
 
 @click.command()
 def main():
     """Generate static sites for resources with none."""
     ontologies = []
-    for cls in ontology_resolver:
-        uri_format = bioregistry.get_uri_format(cls.ontology)
-        if uri_format is None or "biopragmatics.github.io" in uri_format:
-            obo = cls()
-            make_site(obo, HERE.joinpath(obo.ontology))
-            ontologies.append(obo)
+    with logging_redirect_tqdm():
+        for cls in ontology_resolver:
+            uri_format = bioregistry.get_uri_format(cls.ontology)
+            if uri_format is None or "biopragmatics.github.io" in uri_format:
+                obo: Obo = cls()
+                make_site(obo, HERE.joinpath(obo.ontology))
+                ontologies.append(obo)
 
     link_list = "\n".join(
-        f"- [{bioregistry.get_name(obo.ontology)} (`{obo.ontology}`)](/{obo.ontology})"
+        f"- [{bioregistry.get_name(obo.ontology)} (`{obo.ontology}`)]({obo.ontology})"
         for obo in ontologies
     )
     HERE.joinpath("README.md").write_text(f"""\
@@ -40,6 +43,7 @@ The sites can be updated with the following:
 ```shell
 $ pip install tox
 $ tox
+```
 """)
 
 
